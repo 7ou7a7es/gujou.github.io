0b4538e439a316b2a6fa1b34da61c5d858aaf5a2
Guillaume Jourdan <5509339+gujou@users.noreply.github.com>
2021-01-17 12:25:10 +0100

    add js

---
+++ assets/js/default.js
@@ -0,0 +1,20 @@
+var modal = document.getElementById("modal");
+var modalImg = document.getElementById("modal-content");
+var captionText = document.getElementById("modal-caption");
+var span = document.getElementById("modal-close");
+
+var container = userList.querySelector("#main-content");
+var images = container.querySelectorAll("img");
+
+images.forEach((img) => {
+    img.addEventListener('click', () => {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    });
+});
+
+span.addEventListener('click', () => {
+  modal.style.display = "none";
+});
+
